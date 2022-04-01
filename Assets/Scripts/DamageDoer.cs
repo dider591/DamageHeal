0f451c4db5d74d746a6ae8bb2b112e26e5df7b20
ildar v <genanapolotence@gmail.com>
2022-04-01 15:03:15 +0500

    01.04
    15.01

--- Assets/Scripts/DamageDoer.cs
+++ Assets/Scripts/DamageDoer.cs
@@ -14,6 +14,7 @@ public class DamageDoer : MonoBehaviour
     private float _maxHealth = 1f;
     private float _minHealth = 0f;
     private float _damage = 0.1f;
+    private float _speed = 0.1f;
     private float _health;
     private Coroutine _coroutineDamage;
     private Coroutine _coroutineHeal;
@@ -56,11 +57,11 @@ public class DamageDoer : MonoBehaviour
 
         while (_health != target)
         {
-            _health = Mathf.MoveTowards(_health, target, Time.deltaTime * - _damage);
+            _health = Mathf.MoveTowards(_health, target, _speed * Time.deltaTime);
             _slider.value = _health;
-        }
 
-        yield return null;
+            yield return null;
+        }      
     }
 
     private IEnumerable SetHealHealth()
@@ -69,10 +70,10 @@ public class DamageDoer : MonoBehaviour
 
         while (_health != target)
         {
-            _health = Mathf.MoveTowards(_health, target, Time.deltaTime * _damage);
+            _health = Mathf.MoveTowards(_health, target, _speed * Time.deltaTime);
             _slider.value = _health;
-        }
 
-        yield return null;
+            yield return null;
+        }       
     }
 }
